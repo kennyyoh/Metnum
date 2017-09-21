1757e07a83c25b7eed70db5579d47e0185d3d8df
kennyyoh <mine080497@gmail.com>
2017-09-21 19:52:32 +0700

    versi 1.0.0

---
+++ Tugas Metnum1/Kalkulator.vb
@@ -0,0 +1,25 @@
+﻿Public Class Hitung
+
+    Function tambah(ByVal bil1 As Double, ByVal bil2 As Double) As String
+        Return bil1 + bil2
+    End Function
+
+    Function kurang(ByVal bil1 As Double, ByVal bil2 As Double) As String
+        Return bil1 - bil2
+    End Function
+
+    Function bagi(ByVal bil1 As Double, ByVal bil2 As Double) As String
+        Return bil1 / bil2
+    End Function
+
+    Function pangkat(ByVal bil1 As Double, ByVal bil2 As Double) As String
+        Return bil1 ^ bil2
+    End Function
+
+    Function deret(ByVal bil1 As Double, ByVal bil2 As Double) As String
+        Dim x As Integer
+        For bil1 = x To bil2
+        Next
+        Return 3 * (bil1 * bil1) + (1 * bil1) - 1
+    End Function
+End Class
---
+++ Tugas Metnum1/Module1.vb
@@ -0,0 +1,52 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim bil1, bil2 As Double
+        Dim Pil As Integer
+        Dim hit As New Hitung
+
+        Console.WriteLine(" Operasi Hitung ")
+
+
+        Console.WriteLine(" 1. Penambahan ")
+        Console.WriteLine(" 2. Pengurangan ")
+        Console.WriteLine(" 3. Pembagian ")
+        Console.WriteLine(" 4. Pangkat ")
+        Console.WriteLine(" 5. Deret ")
+        Console.Write(" Pilihan : ")
+
+
+
+        Pil = Console.ReadLine
+        Console.Clear()
+        Console.Write(" Masukkan angka 1 : ")
+        bil1 = Console.ReadLine()
+
+        Console.Write(" Masukan angka 2  : ")
+        bil2 = Console.ReadLine()
+
+        Console.WriteLine()
+
+
+        Select Case UCase(Pil)
+            Case "1"
+                Console.Write(" Hasil Penambahan  : " & hit.tambah(bil1, bil2))
+                Console.ReadLine()
+            Case "2"
+                Console.Write(" Hasil Pengurangan : " & hit.kurang(bil1, bil2))
+                Console.ReadLine()
+            Case "3"
+                Console.Write(" Hasil Pembagian   : " & hit.bagi(bil1, bil2))
+                Console.ReadLine()
+            Case "4"
+                Console.WriteLine(" Hasil Pangkat : " & hit.pangkat(bil1, bil2))
+                Console.ReadLine()
+            Case "5"
+                Console.WriteLine(" Hasil Deret   : " & hit.deret(bil1, bil1))
+                Console.WriteLine(" Hasil Deret   : " & hit.deret(bil2, bil2))
+                Console.ReadLine()
+        End Select
+
+    End Sub
+
+End Module
